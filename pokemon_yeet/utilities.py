6a890f82ea26061754728678c4f51623c223edfd
2ndBillingCycle <2nd@2ndbillingcycle.stream>
2021-03-03 08:46:55 -0600

    refactor(utilities.py): use pathlib, break up big try / catch

--- pokemon_yeet/utilities.py
+++ pokemon_yeet/utilities.py
@@ -4,38 +4,62 @@ utility functions
 caches pokemon data
 """
 import requests
+import pathlib
 import json
-import os
+from requests.exceptions import ConnectionError
+from json.decoder import JSONDecodeError
+from .constants import pokedex_number_start, pokedex_number_end
+
+cache_directory = pathlib.Path("./pokemon")
 
 def setup_game():
     "download pokemon data"
-    if os.path.isdir('./pokemon') == False:
-        print("Creating pokemon directory...")
-        os.mkdir('./pokemon')
+    if not cache_directory.is_dir():
+        print(f"Creating cache directory '{cache_directory.resolve()}'...")
+        cache_directory.mkdir()
 
     for i in range(1, 152):
-        if os.path.isfile(f'./pokemon/{i}.json') == False:
-            print("Importing Pokemon....")
-            try:
-                r = requests.get(f'https://pokeapi.co/api/v2/pokemon/{i}')
-                f = open(f"./pokemon/{i}.json", "a")
-                f.write(r.text)
-                f.close()
+        pokemon_json_file = cache_directory / f"{i}.json"
 
-            except:
-                print(f"Poke Number: {i} not found")
-                continue
-    
-    print("Game is Setup and Ready to begin")
+        if pokemon_json_file.is_file():
+            # Pokemon already downloaded
+            continue
+
+        print(f"Downloading pokemon #{i}")
+
+        error_message = f"cannot get pokemon #{i}"
+        try:
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{i}')
+        except:
+            print(error_message)
+            continue
+        finally:
+            response.close()
+
+        if response.status_code != 200:
+            print(error_message)
+            continue
+
+        try:
+            pokemon_json = response.json()
+        except JSONDecodeError:
+            print(f"cannot decode json for pokemon #{i}")
+            continue
+
+        with pokemon_json_file.open(mode="wt") as file:
+            json.dump(pokemon_json, file, indent=2, sort_keys=True)
+
+    print("Pokemon data downloaded")
 
 
-def load_pokemon(pokemon):
+def load_pokemon(pokemon_number):
     "return the information for one pokemon"
-    with open(f'./pokemon/{pokemon}.json') as f:
-        data = json.load(f)
+    pokemon_json_file = cache_directory / f"{pokemon_number}.json"
+    with pokemon_json_file.open(mode="rt") as file:
+        data = json.load(file)
 
-        poke_name = data['name']
-        poke_type = data['types'][0]['type']['name']
-        poke_hp = data['stats'][0]['base_stat']
+    poke_name = data['name']
+    poke_type = data['types'][0]['type']['name']
+    poke_hp = data['stats'][0]['base_stat']
 
     return poke_name, poke_type, poke_hp
